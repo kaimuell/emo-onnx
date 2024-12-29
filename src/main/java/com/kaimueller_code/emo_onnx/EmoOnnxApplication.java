266426399d4d3502e2230ea8fbd234347404d3a1
Kai Mueller <me@kaimueller-code.com>
2024-12-29 15:50:29 +0100

    initial commit

---
+++ src/main/java/com/kaimueller_code/emo_onnx/EmoOnnxApplication.java
@@ -0,0 +1,13 @@
+package com.kaimueller_code.emo_onnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoOnnxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoOnnxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaimueller_code/emo_onnx/EmoOnnxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaimueller_code.emo_onnx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoOnnxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
