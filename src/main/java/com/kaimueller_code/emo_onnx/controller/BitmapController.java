90c533b652072ee4e5a35f7858e6cd34cd660460
Kai Mueller <me@kaimueller-code.com>
2024-12-31 17:09:09 +0100

    add html page as frontend

--- src/main/java/com/kaimueller_code/emo_onnx/controller/BitmapController.java
+++ src/main/java/com/kaimueller_code/emo_onnx/controller/BitmapController.java
@@ -3,10 +3,11 @@ package com.kaimueller_code.emo_onnx.controller;
 
 import ai.onnxruntime.OrtException;
 import com.kaimueller_code.emo_onnx.service.EmotionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +20,8 @@ import java.io.IOException;
 @RestController
 public class BitmapController {
 
+    Logger logger = LoggerFactory.getLogger(BitmapController.class);
+
     @Autowired
     EmotionService service;
 
@@ -26,6 +29,7 @@ public class BitmapController {
     public ResponseEntity<String> postImageForEmotion(@RequestParam("file") MultipartFile multipartFile) throws IOException {
 
         try {
+            logger.info("recieved file");
             if (multipartFile.isEmpty()){
                 return ResponseEntity.badRequest().body("empty file");
             }
@@ -35,6 +39,7 @@ public class BitmapController {
             }
             // Process the image (e.g., save to disk, process content, etc.)
             String emotion = service.inferEmotion(image);
+            logger.info("infered emoton: " + emotion);
             return ResponseEntity.ok(emotion);
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Failed to upload the file.");
